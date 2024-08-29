6b8744cdf1ff41d4dcab4e638a172aac8a47e0b3
Yuma Soga <primary@yum.onl>
2024-08-29 13:30:53 +0900

    feat: basic funcs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yum45f/multicodec
+
+go 1.23.0
---
+++ multicodec.go
@@ -0,0 +1,30 @@
+package multicodec
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+const (
+	MaxLenUvarint63   = 9
+	MaxValueUvarint63 = (1 << 63) - 1
+)
+
+const (
+	P256Pub = 0x1200
+)
+
+func DecodeMulticodec(multicodec []byte) (uint64, []byte, error) {
+	code, n := binary.Uvarint(multicodec)
+	if n <= 0 {
+		return 0, nil, fmt.Errorf("invalid multicodec; varint overflow")
+	}
+
+	return code, multicodec[n:], nil
+}
+
+func EncodeMulticodec(code uint64, bytes []byte) []byte {
+	buf := make([]byte, binary.MaxVarintLen64)
+	n := binary.PutUvarint(buf, code)
+	return append(buf[:n], bytes...)
+}
